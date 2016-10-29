d39dd428a9758565ccb3586b729bdab5eb01b0e2
HunterMeyer <huntermeyer@live.com>
2016-10-29 16:44:02 -0400

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { AppRegistry, NativeModules } from 'react-native'
+import AppNavigator from './android/app/navigation/AppNavigator'
+import { COLOR, ThemeProvider } from './node_modules/react-native-material-ui'
+
+export default class Circle extends Component {
+  render() {
+    return (
+      <ThemeProvider uiTheme={uiTheme}>
+        <AppNavigator />
+      </ThemeProvider>
+    )
+  }
+}
+
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.indigo500,
+    accentColor: COLOR.greenA700
+  },
+  toolbar: {
+    container: {
+      height: 50
+    }
+  }
+}
+
+AppRegistry.registerComponent('Circle', () => Circle);
