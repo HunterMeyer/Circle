9adb4a1046b738dcbb9de98dd59736d5e4eee742
HunterMeyer <huntermeyer@live.com>
2016-10-30 01:06:56 -0400

    Add StatusBar to Main

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -1,7 +1,8 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { Toolbar } from 'react-native-material-ui'
+import { StatusBar } from 'react-native'
+import { Toolbar, COLOR } from 'react-native-material-ui'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 import Container from '../shared/Container'
 import TabBar from '../shared/TabBar'
@@ -9,12 +10,17 @@ import FeedTab from './FeedTab'
 import TagsTab from './TagsTab'
 import NotificationsTab from './NotificationsTab'
 
-export default class Stream extends Component {
+export default class Main extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <Container>
+        <StatusBar backgroundColor={ COLOR.indigo800 } />
         <Toolbar
-          centerElement='Circle'
+          centerElement={ this.props.title }
           searchable={{
             autoFocus: true,
             placeholder: 'Location...'
@@ -22,11 +28,13 @@ export default class Stream extends Component {
           style={{ container: { elevation: 0 } }}
         />
         <ScrollableTabView renderTabBar={() => <TabBar />} >
-          <FeedTab tabLabel='place' />
-          <TagsTab tabLabel='style' />
-          <NotificationsTab tabLabel='notifications' />
+          <FeedTab tabLabel='place' { ...this.props } />
+          <TagsTab tabLabel='style' { ...this.props } />
+          <NotificationsTab tabLabel='notifications' { ...this.props } />
         </ScrollableTabView>
       </Container>
     )
   }
 }
+
+module.exports = Main
