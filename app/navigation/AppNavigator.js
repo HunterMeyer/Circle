ccbde147e352f386276f231f597805afd88c10c4
HunterMeyer <huntermeyer@live.com>
2016-10-30 21:38:54 -0400

    Detect AndroidBack button press

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,10 +1,20 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { Navigator, StyleSheet } from 'react-native'
+import { Navigator, StyleSheet, BackAndroid } from 'react-native'
 import { COLOR } from 'react-native-material-ui';
 import Container from '../shared/Container'
 import Main from '../screens/Main'
+
+var navigator
+
+BackAndroid.addEventListener('hardwareBackButtonPress', () => {
+  if (navigator && navigator.getCurrentRoutes().length > 1) {
+    navigator.pop()
+    return true
+  }
+  return false
+})
 
 export default class AppNavigator extends Component {
   _renderScene(route, navigator) {
@@ -18,6 +28,7 @@ export default class AppNavigator extends Component {
   render() {
     return (
       <Navigator
+        ref={(nav) => { navigator = nav }}
         style={ styles.container }
         initialRoute={{ screen: Main, passProps: { title: 'Circle' }}}
         renderScene={ this._renderScene }
